03d602a5382779d2d7b77559a5e310bfa700c1d0
Josue N Rivera <josuevaldez1.com@outlook.com>
2018-12-30 03:42:34 -0500

    Remove ends

--- lib/ionicons/links.php
+++ lib/ionicons/links.php
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 <?php
   $links = array(
     'css' => 'lib/ionicons/css/ionicons.min.css'
   );
 ?>
-=======
-<?php
-  $links = array(
-    'css' => 'lib/ionicons/css/ionicons.min.css'
-  );
-?>
->>>>>>> 89da60dae3fa4c1088346536d22a4c7344f2e0ea
