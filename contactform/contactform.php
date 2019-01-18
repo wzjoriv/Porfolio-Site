dc13e97b8978f3c16ca5667f3b14b595acdfb36f
Josue N Rivera <josuevaldez1.com@outlook.com>
2019-01-17 23:47:21 -0500

    Patch to Contactform
    Created a better format for the emails

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,36 +1,16 @@
-<<<<<<< HEAD
+
 <?php
 
-$name = $_POST["name"];
+$to = "Josue <josuevaldez1.com@outlook.com>";
 
-$sub = "Email from josuenrivera.site by " . $name;
+$subject = "Porfolio: " . $_POST["subject"];
 
-$headers = "From: " . $_POST["email"];
+$msg = wordwrap($_POST["message"], 70, "\r\n");
 
-$msg = wordwrap("Subject(" . $_POST["subject"] . "):\n" . $_POST["message"], 70);
+$headers = "From: " . $_POST["name"] . " <" . $_POST["email"] . ">\r\n";
 
-$to = "josuevaldez1.com@outlook.com";
+mail($to, $subject, $msg, $headers);
 
-mail($to, $sub, $msg, $headers);
+echo 'OK';
 
-echo "OK";
-
-=======
-<?php
-
-$name = $_POST["name"];
-
-$sub = "Email from josuenrivera.site by " . $name;
-
-$headers = "From: " . $_POST["email"];
-
-$msg = wordwrap("Subject(" . $_POST["subject"] . "):\n" . $_POST["message"], 70);
-
-$to = "josuevaldez1.com@outlook.com";
-
-mail($to, $sub, $msg, $headers);
-
-echo "OK";
-
->>>>>>> 89da60dae3fa4c1088346536d22a4c7344f2e0ea
 ?>
