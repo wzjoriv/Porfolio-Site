c2fc43f480168ef415e78466505ddcf1abeebc2d
Josue N Rivera <josue.n.rivera@outlook.com>
2020-06-18 03:15:11 -0400

    Masonry added
    - Need to fix photosSlides

--- js/main.js
+++ js/main.js
@@ -12,39 +12,39 @@ $(document).ready(function() {
   $(document).on("scroll", onScroll);
 
   $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
+	e.preventDefault();
+	$(document).off("scroll");
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
-    });
+	$('a').each(function() {
+	  $(this).removeClass('active');
+	  if ($(window).width() < 768) {
+		$('.nav-menu').slideUp();
+	  }
+	});
 
-    $(this).addClass('active');
+	$(this).addClass('active');
 
-    var target = this.hash,
-        menu = target;
+	var target = this.hash,
+		menu = target;
 
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
+	target = $(target);
+	$('html, body').stop().animate({
+	  'scrollTop': target.offset().top - 80
+	}, 500, 'swing', function() {
+	  window.location.hash = target.selector;
+	  $(document).on("scroll", onScroll);
+	});
   });
 
 
   function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a .smoothScroll').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
+	if ($('.home').length) {
+	  var scrollPos = $(document).scrollTop();
+	  $('nav ul li a .smoothScroll').each(function() {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+	  });
+	}
   }
 
   // ========================================================================= //
@@ -53,15 +53,15 @@ $(document).ready(function() {
 
 
   $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
+	var scroll = $(window).scrollTop();
+	if (scroll > 200 ) {
+	  $("#main-nav, #main-nav-subpage").slideDown(700);
+	  $("#main-nav-subpage").removeClass('subpage-nav');
+	} else {
+	  $("#main-nav").slideUp(700);
+	  $("#main-nav-subpage").hide();
+	  $("#main-nav-subpage").addClass('subpage-nav');
+	}
   });
 
   // ========================================================================= //
@@ -69,7 +69,7 @@ $(document).ready(function() {
   // ========================================================================= //
 
   $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
+	$('.nav-menu').slideToggle();
   });
 
   // ========================================================================= //
@@ -79,14 +79,14 @@ $(document).ready(function() {
   var typed = $(".typed");
 
   $(function() {
-    typed.typed({
-      strings: ["Technology.", "Computer Science.", "Machine Learning.", 
-      "Software Engineering.", "Understanding intelligence.",
-      "Understanding the universe.", "Civic Engagement.", "Life.", "Art.", "Music.",
-      "Mathematics.", "Science.", "Physics.", "Learning.", "And the future."],
-      typeSpeed: 100,
-      loop: true,
-    });
+	typed.typed({
+	  strings: ["Technology.", "Computer Science.", "Machine Learning.", 
+	  "Software Engineering.", "Understanding intelligence.",
+	  "Understanding the universe.", "Civic Engagement.", "Life.", "Art.", "Music.",
+	  "Mathematics.", "Science.", "Physics.", "Learning.", "And the future."],
+	  typeSpeed: 100,
+	  loop: true,
+	});
   });
 
 
@@ -96,33 +96,35 @@ $(document).ready(function() {
 
 
   $('.facts-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      autoplaySpeed: 1000,
-      autoplayTimeout: 13000,
-      margin: 20,
-      dots: true,
-      nav: true,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
+	  autoplay: true,
+	  loop: true,
+	  autoplaySpeed: 1000,
+	  autoplayTimeout: 13000,
+	  margin: 20,
+	  dots: true,
+	  nav: true,
+	  responsiveClass: true,
+	  responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
+	});
 
 
   // ========================================================================= //
-  //  Porfolio isotope and filter
+  //  Porfolio isotope and filter, Masonry
   // ========================================================================= //
 
-
+  var msnry = new Masonry( '.portfolio-container', {
+	itemSelector: '.portfolio-thumbnail'
+  });
+  
   var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
+	itemSelector: '.portfolio-thumbnail'
   });
 
   $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+	$("#portfolio-flters li").removeClass('filter-active');
+	$(this).addClass('filter-active');
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+	portfolioIsotope.isotope({ filter: $(this).data('filter') });
   });
 
 
@@ -130,46 +132,53 @@ $(document).ready(function() {
   //  magnificPopup
   // ========================================================================= //
 
-  var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery: {
-        enabled: true
-      },
-      image: {
-        markup: '<div class="mfp-figure">'+
-            '<div class="mfp-close"></div>'+
-            '<div class="mfp-img"></div>'+ // Floated left 
-            '<div class="mfp-bottom-bar">'+
-              '<div class="mfp-counter"></div>'+
-              '<div class="mfp-title"></div>'+
-            '</div>'+
-          '</div>',
-          cursor: 'mfp-zoom-out-cur',
-          tError: '<a href="%url%">The image</a> could not be loaded.'
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
+  var items = [],
+	  $pswp = $('.pswp')[0],
+	  $folioItems = $('.item-folio');
 
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+  // get items
+  $folioItems.each( function(i) {
 
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
-    });
-  };
+	var $folio = $(this),
+		$thumbLink =  $folio.find('.thumb-link'),
+		$title = $folio.find('.item-folio__title'),
+		$caption = $folio.find('.item-folio__caption'),
+		$titleText = '<h4>' + $.trim($title.html()) + '</h4>',
+		$captionText = $.trim($caption.html()),
+		$href = $thumbLink.attr('href'),
+		$size = $thumbLink.data('size').split('x'),
+		$width  = $size[0],
+		$height = $size[1];
 
-  // Call the functions
-  magnifPopup();
+	var item = {
+		src  : $href,
+		w    : $width,
+		h    : $height
+	}
+
+	  if ($caption.length > 0) {
+		  item.title = $.trim($titleText + $captionText);
+	  }
+
+	  items.push(item);
+  });
+
+  // bind click event
+  $folioItems.each(function(i) {
+
+	$(this).on('click', function(e) {
+		  e.preventDefault();
+		  var options = {
+			  index: i,
+			  showHideOpacity: true
+		  }
+
+		  // initialize PhotoSwipe
+		  var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+		  lightBox.init();
+  	});
+
+  });
 
 });
 
